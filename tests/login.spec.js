d19d565b0fa91f7cc171996eceabbdd1df5ac751
Konrad Kuźniak <115842045+Kuzniakers@users.noreply.github.com>
2024-03-05 20:42:41 +0100

    init repo

---
+++ tests/login.spec.js
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+
+test.describe('User login to Demobank', () => {
+
+  test('successful login with correct credentials', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').fill('testerlo');
+    await page.getByTestId('password-input').fill('12345678');
+    await page.getByTestId('login-button').click();
+
+    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+  });
+
+  test('unsuccessful login with too short username', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').fill('tester');
+    await page.getByTestId('password-input').click();
+
+    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+  });
+  
+  test('unsuccessful login with too short password', async ({ page }) => {
+  await page.goto('https://demo-bank.vercel.app/');
+  await page.getByTestId('login-input').fill('testerlo');
+  await page.getByTestId('password-input').fill('1234');
+  await page.getByTestId('password-input').blur();
+
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+  });
+
+});
---
+++ tests/pulpit.spec.js
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Pulpit test', () => {
+
+    test('test', async ({ page }) => {
+        await page.goto('https://demo-bank.vercel.app/pulpit.html');
+        await page.getByTestId('login-input').fill('testerlo');
+        await page.getByTestId('password-input').fill('12345678');
+        await page.getByTestId('login-button').click();
+
+        await page.locator('#widget_1_transfer_receiver').selectOption('2');
+        await page.locator('#widget_1_transfer_amount').fill('150');
+        await page.locator('#widget_1_transfer_title').fill('Zwrot środków');
+
+        await page.locator('#execute_btn').click();
+        await page.getByTestId('close-button').click();
+
+        
+      });
+
+      test.only('successful mobile top-up', async ({page}) => {
+
+        await page.goto('https://demo-bank.vercel.app/pulpit.html');
+        await page.getByTestId('login-input').fill('testerlo');
+        await page.getByTestId('password-input').fill('12345678');
+        await page.getByTestId('login-button').click();
+
+        await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
+        await page.locator('#widget_1_topup_amount').fill('40');
+        await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.getByRole('button', { name: 'doładuj telefon' }).click();
+        await page.getByTestId('close-button').click();
+
+        await expect(page.locator('#show_messages')).toHaveText('Doładowanie wykonane! 40,00PLN na numer 500 xxx xxx')
+      });
+      
+});
